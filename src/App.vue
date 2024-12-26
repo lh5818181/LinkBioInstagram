36c85566b95d66ad1a5c476652987480cece6e8b
Luis Henrique <luish7572@gmail.com>
2024-12-26 14:18:03 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <img src="../public/profile.jpg" alt="Foto de perfil" class="profile-picture" />
+      <h1>Meus Links</h1>
+      <p class="professional-name">@devriqueoliver</p>
+      <LinksList :links="links" />
+    </div>
+    <footer class="container footer">
+      &copy; 2024 Todos os direitos reservados | Luis Henrique Vieira de Oliveira
+    </footer>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import LinksList from "./components/LinksList.vue";
+
+// Tipagem para os Links
+interface Link {
+  id: number;
+  label: string;
+  url: string;
+}
+
+// Lista de links
+const links: Link[] = [
+  { id: 1, label: "Portfolio Web", url: "https://portfolio-home-ten.vercel.app/" },
+  { id: 2, label: "GitHub", url: "https://github.com/lh5818181" },
+  { id: 3, label: "LinkedIn", url: "https://www.linkedin.com/in/luis-henrique-76245231a/" },
+  { id: 4, label: "Whatsapp", url: "https://wa.me/qr/XOOHIGLGNEOCA1" }
+];
+</script>
+
+<style src="./styles/global.css"></style>
---
+++ src/components/LinksList.vue
@@ -0,0 +1,44 @@
+    <template>
+        <ul class="links-list">
+        <li v-for="link in links" :key="link.id">
+            <a :href="link.url" target="_blank" rel="noopener noreferrer">
+            {{ link.label }}
+            </a>
+        </li>
+        </ul>
+    </template>
+    
+    <script lang="ts" setup>
+    // Tipagem para os Links
+    interface Link {
+        id: number;
+        label: string;
+        url: string;
+    }
+    
+    // Props recebidas do componente pai
+    defineProps<{ links: Link[] }>();
+    </script>
+    
+    <style scoped>
+    .links-list {
+        list-style-type: none;
+        padding: 0;
+    }
+    
+    .links-list li {
+        margin: 10px 0;
+    }
+    
+    .links-list a {
+        text-decoration: none;
+        font-size: 18px;
+        color: #2575fc;
+        font-weight: bold;
+    }
+    
+    .links-list a:hover {
+        color: #1a5bb8;
+    }
+    </style>
+    
